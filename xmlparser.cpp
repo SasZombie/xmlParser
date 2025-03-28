0fb20d04aba8fb7dcfe9046d1e5e6fca98a08db9
SasZombie <gigelusas81@gmail.com>
2025-03-28 13:02:12 +0200

    commit

--- xmlparser.cpp
+++ xmlparser.cpp
@@ -4,6 +4,29 @@
 namespace xmlParser
 {
 
+    // tagName
+    // tagName siAdnorato="BLaLBalBla"
+    // tagNames siAdnorato="BLaLBalBla"
+    // Where, what
+    static bool contains(std::string_view str1, std::string_view str2) noexcept
+    {
+        if (str1 == str2)
+            return true;
+        size_t len1 = str1.length(), len2 = str2.length();
+     
+        if (len1 <= len2)
+            return false;
+
+        size_t ind = 0;
+
+        while (ind < len2 && str1[ind] == str2[ind])
+        {
+            ++ind;
+        }
+        
+        return (ind == len1 && str1[ind + 1] == ' ');
+    }
+    
     void xmlNode::findAll(std::vector<std::shared_ptr<xmlNode>> &results) const noexcept
     {
         for (const auto &child : nodes)
@@ -15,7 +38,8 @@ namespace xmlParser
 
     void xmlNode::findNode(std::string_view target, std::vector<std::shared_ptr<xmlNode>> &results) noexcept
     {
-        if (tagName.contains(target))
+        // if (tagName.contains(target))
+        if(contains(tagName, target))
         {
             results.push_back(shared_from_this());
             findAll(results);
@@ -32,11 +56,10 @@ namespace xmlParser
         std::vector<std::shared_ptr<xmlNode>> results;
         size_t len = target.size();
 
-        if(target[0] == '<' && target[len-1] == '>')
-            findNode(target.substr(1, len-2) , results);
+        if (target[0] == '<' && target[len - 1] == '>')
+            findNode(target.substr(1, len - 2), results);
         else
             findNode(target, results);
-
 
         return results;
     }
@@ -86,9 +109,9 @@ namespace xmlParser
         std::vector<xmlToken> tokens;
 
         std::getline(input, line);
-        //XML version
-        if(line.size() > 2 && line[1] == '?' && line.back() == '>') 
-        {  
+        // XML version
+        if (line.size() > 2 && line[1] == '?' && line.back() == '>')
+        {
             tokens.push_back({TokenType::META, line});
 
             currsor = input.tellg();
@@ -99,9 +122,9 @@ namespace xmlParser
         }
 
         std::getline(input, line);
-        //Doctype version
-        if(line.find("DOCTYPE") != std::string::npos) 
-        {  
+        // Doctype version
+        if (line.find("DOCTYPE") != std::string::npos)
+        {
             tokens.push_back({TokenType::META, line});
         }
         else
@@ -137,7 +160,7 @@ namespace xmlParser
                         ++closeTokens;
                         currentType = TokenType::TAG_CLOSE;
                         // Dropping the </ and >
-                        tokens.push_back({currentType, line.substr(start + 2, size - 3)});
+                        tokens.push_back({currentType, line.substr(start + 1, size - 2)});
                     }
                     else
                     {
@@ -191,7 +214,7 @@ namespace xmlParser
 
         std::vector<xmlToken> tokens = tokenizeString(input);
 
-        if(tokens.size() == 0)
+        if (tokens.size() == 0)
         {
             std::cerr << "File is empty!\n";
             return std::make_shared<xmlNode>();
@@ -201,8 +224,8 @@ namespace xmlParser
         std::shared_ptr<xmlNode> root = std::make_shared<xmlNode>();
         std::shared_ptr<xmlNode> current = root;
 
-        //Ugly ahh syntax for skipping 1 elem :)
-        for (const auto &[kind, value] : tokens /* | std::ranges::views::drop(1) */) 
+        // Ugly ahh syntax for skipping 1 elem :)
+        for (const auto &[kind, value] : tokens /* | std::ranges::views::drop(1) */)
         {
             switch (kind)
             {
@@ -236,11 +259,11 @@ namespace xmlParser
                 break;
             }
 
-            // case TokenType::META:
-            // {
-            //     std::shared_ptr<xmlNode> child = std::make_shared<xmlNode>(kind, value, current);
+                // case TokenType::META:
+                // {
+                //     std::shared_ptr<xmlNode> child = std::make_shared<xmlNode>(kind, value, current);
 
-            // }
+                // }
 
             default:
                 std::cerr << "Unreachable! How did we get here?\n";
